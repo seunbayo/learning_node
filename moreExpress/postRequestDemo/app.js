5e0eea17ba30267646d4c78d7c9a7e91edd722f3
seunbayo <seunbayo83@gmail.com>
2020-07-19 17:54:08 +0100

    added a form to retrieve saved data from the body of the form

--- moreExpress/postRequestDemo/app.js
+++ moreExpress/postRequestDemo/app.js
@@ -1,30 +1,29 @@
 var express = require("express")
 var app = express();
-var bodyparser = require("body-parser");
+var bodyParser = require("body-parser");
 
 // To use body parser
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'))
-
 app.set("view engine", "ejs");
+var friends = ["Tony","john","sam","lexxy"];
 
 // create a route for the home page
 app.get("/", function(req, res){
     res.render("home");
 });
 
-app.post("/addfriend", function(req, res){
-    console.log('req.body');
-    
-    res.send("you have reach the post route!!");
+// setting up a post route
+app.post("/addfriend", function(req, res) {
+    var newFriend = req.body.newfriend;
+    friends.push(newFriend);
+    res.redirect("/friends");
 })
 
 // Create a friend route
 app.get("/friends", function(req, res){
-    var friends = ["Tony","john","sam","lexxy"];
     res.render("friends", {friends: friends});
 });
-
 
 
 app.listen(3000, function(){
